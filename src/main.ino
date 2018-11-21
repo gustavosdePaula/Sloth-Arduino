12985167dbec8cc9847a1e6fdbf66f038f71d657
Gustavo S. de Paula <gustavo.silva7337@gmail.com>
2018-11-21 20:45:33 -0200

    qtr sensor lib add

--- src/main.ino
+++ src/main.ino
@@ -3,7 +3,7 @@
 #include "Motor.h"
 
 //#include "PID.h"
-//#include "Encoder/Encoder.h"
+#include "Encoder.h"
 //#include "QTRSensors.h"
 
 
@@ -17,6 +17,9 @@
 //Objects
 Motor LeftMotor(PIN_M1_PWM, PIN_M1_IN1, PIN_M1_IN2); // Left Motor
 Motor RightMotor(PIN_M2_PWM, PIN_M2_IN1, PIN_M2_IN2); // Right Motor
+
+Encoder LeftEncoder(PIN_ENC1_A, PIN_ENC1_B, PULSES_PER_REV, WHEEL_RADIUS);
+Encoder RightEncoder(PIN_ENC2_A, PIN_ENC2_B, PULSES_PER_REV, WHEEL_RADIUS);
 
 
 
@@ -35,4 +38,6 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 	
+	float pulsesL = LeftEncoder.getPulses();
+	float pulsesR = RightEncoder.getPulses();
 }
